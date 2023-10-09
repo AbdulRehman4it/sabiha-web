505a19c4bacd3d7b7beb6bf466aeec76d6f6724a
IqraShafaqat <iqrashafaqat032768@gmail.com>
2023-10-09 16:22:20 +0500

    Your commit message here new

--- resources/views/home/homepage.blade.php
+++ resources/views/home/homepage.blade.php
@@ -65,8 +65,7 @@
                   <span class="input-group-text" id="basic-addon2"
                     >Subscribe</span
                   >
-                  <span>Hello World</span> 
-                  <span>Hello World</span> 
+                  <span>Hello World to</span> 
                 </div>
               
               </div>
